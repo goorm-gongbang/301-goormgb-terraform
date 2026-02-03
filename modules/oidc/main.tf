5137c3527df796ef586e3085fe5a9fecf05e6c70
정재형 <chamchi0129@gmail.com>
2026-02-03 10:59:40 +0900

    주석 수정

--- modules/oidc/main.tf
+++ modules/oidc/main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
 
-  # GitHub 서버의 인증서 지문 (AWS 공식 가이드 값)
+  # GitHub 서버의 인증서 지문
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
 }
 
