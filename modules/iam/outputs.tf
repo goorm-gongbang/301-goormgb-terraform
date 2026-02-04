9377ad67132ffe2648660930e0e0d162f8c21070
212clab <212clab@gmail.com>
2026-02-04 23:10:43 +0900

    IAM. github arn 추가

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -5,6 +5,11 @@ output "groups" {
 }
 
 # CI/CD Role ARNs
+output "github_actions_role_arn" {
+  description = "GitHub Actions IAM Role ARN"
+  value       = aws_iam_role.github_actions.arn
+}
+
 output "teamcity_role_arn" {
   description = "TeamCity IAM Role ARN"
   value       = aws_iam_role.teamcity.arn
